33d9f6522f166e595bb9b8013ddc0c9729b39bea
Anup kumar <anupkumarpandey@outlook.com>
2020-10-25 15:16:25 +0530

    new commit

---
+++ anup6.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+	printf("enter the marks of the student:\n");
+	scanf("%d" <= 100)
+{
+	printf("please enter valid marks, i.e. out of 100\n");
+	printf("enter the marks of the student: ");
+	scanf("%d", &marks);
+}
+else
+{
+	if (marks >= 90)
+	{
+		printf("Grade: A+\n");
+	}
+	else if (marks >= 80)
+	{
+		printf("grade: A\n");
+	}
+	else if (marks >=70)
+	{
+		print("Grade: B\n");
+	}
+	elseif (marks >=60)
+	{
+		printf("Grade: C\n");
+	}
+	else if (marks >= 50)
+	{
+		printf("Grade: D\n");
+	}
+	else if (marks >= 40)
+	{
+		printf("Grade: E\n");
+	}
+	else
+	{
+		printf("Fail\n");
+	}
+}
+return 0;
